2d798c202c38a70d15149025ec2fc644b245787e
igic01 <igic.nikola01@gmail.com>
2023-10-11 20:49:07 +0300

    finally back to my acc, and little bit of codewars with java

---
+++ Java/Hex.java
@@ -0,0 +1,8 @@
+public class Hex {
+    public static void main(String[] args) {
+        // System.out.println(whoLikesIt());
+        String str = String.format("%02X",255);
+        System.out.println(str);
+
+    }
+}
--- Java/Test.java
+++ Java/Test.java
@@ -1,9 +1,25 @@
 import java.util.*;
 class Test{
-    public static void main(String[] args) {
-        // System.out.println(whoLikesIt());
-        String str = String.format("%02X",255);
-        System.out.println(str);
 
+    static long multiple_dig(long number){
+        long result = 1;
+        while (number != 0) {
+            long digit = number % 10;
+            result *= digit;
+            number /= 10;
+        }
+        return result;
+    }
+
+    static int count_till_single_digit(long number){
+        int count = 0;
+        while( ("" + number).length() > 1 ){
+            number = multiple_dig(number);
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(count_till_single_digit(999));
     }
 }
